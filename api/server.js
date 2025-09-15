1a4e78d9ba1081d893b0013260b4c36193331453
ZhanboGong <z.gong.11@student.scu.edu.au>
2025-09-15 15:20:18 +0800

    Update server.js
    Do the same thing as was done in the previous commit.

--- api/server.js
+++ api/server.js
@@ -3,4 +3,17 @@ const express = require("require");
 const bodyPaser = require("body-parser");
 const api_controller = require("./api-controller/api-controller");
 const path = require("path");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// 导入HTML
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, ""));
+});
+
+//路径待修改
+app.use("/api/events", api_controller);
