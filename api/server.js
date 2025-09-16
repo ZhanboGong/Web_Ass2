83b516379baa98bd685e3b4d9102c368df17a0c3
ZhanboGong <z.gong.11@student.scu.edu.au>
2025-09-16 22:09:07 +0800

    Binding HTML
    Binding index, search, login, signup html

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path'); // ✅ 引入 path 模块
+const path = require('path');
 const apiController = require('./api-controller/api-controller');
 
 const app = express();
@@ -12,19 +12,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../clientside')));
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../clientside/HTML/index.html'));
 });
 
-app.get('/search', (_req, res) => {
+app.get('/search', (req, res) => {
     res.sendFile(path.join(__dirname, '../clientside/HTML/search.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../clientside/HTML/login.html'))
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, '../clientside/HTML/signup.html'))
 });
 
 app.use('/api', apiController);
 
 
-app.use((_req, res) => res.status(404).json({ msg: 'Route not found' }));
+app.use((req, res) => res.status(404).json({ msg: 'Route not found' }));
 
-// 启动
+
 const PORT = 3030;
-app.listen(PORT, () => console.log(`✅ Server ready: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server ready: http://localhost:${PORT}`));
